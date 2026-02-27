16b686cf8035225e4fce0358eb0917e5a1841540
Awesome Rustacean <geniusinrust@gmail.com>
2026-02-27 22:29:25 +0300

    Add Tokens for the language

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -1,6 +1,89 @@
 use crate::reader::Source;
+use std::collections::HashMap;
 
-pub struct Tokens {}
+#[derive(Debug)]
+pub enum TokenType {
+    // Single character tokens
+    LeftParen,
+    RightParen,
+    LeftBraces,
+    RightBraces,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    SemiColon,
+    Slash,
+    Star,
+    // One or Two Character Token
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    // Literals
+    Identifier,
+    String,
+    Number,
+    Float,
+    // Keywords
+    Class,
+    And,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    While,
+    Print,
+    Super,
+    This,
+    True,
+    Var,
+    //
+    EOF,
+}
+
+#[derive(Debug)]
+pub struct Token {
+    toktype: TokenType,
+    lexeme: String,
+    literal: Literal,
+    // literal: object
+    line: usize,
+}
+
+#[derive(Debug)]
+pub enum Literal {
+    Str(String),
+    Num(f64),
+    None,
+}
+
+impl Token {
+    pub fn new(toktype: TokenType, lexeme: String, line: usize, literal: Literal) -> Self {
+        Self {
+            toktype,
+            lexeme,
+            line,
+            literal,
+        }
+    }
+
+    pub fn to_string(self) -> String {
+        return format!("{:#?} {} {:#?}", self.toktype, self.lexeme, self.literal);
+    }
+}
+
+#[derive(Debug)]
+pub struct Tokens {
+    tokens: Vec<Token>,
+}
 
 #[derive(Debug)]
 pub struct TError {}
@@ -8,7 +91,9 @@ pub struct TError {}
 pub fn tokenize(source: Source) -> Result<Tokens, TError> {
     println!("Tokenize");
 
-    Ok(Tokens {})
+    let tokens = vec![];
+
+    Ok(Tokens { tokens })
 }
 
 #[cfg(test)]
