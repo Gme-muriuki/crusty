d7be641adc4043b3c3c45ec06c7fc3ce0215d7d9
Awesome Rustacean <geniusinrust@gmail.com>
2026-02-27 21:37:16 +0300

    Add REPL loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 #![allow(unused)]
-use crate::{evaluate::evaluate, parser::parse, reader::read_source, tokenize::tokenize};
+use std::{
+    env,
+    io::{Write, stdin, stdout},
+    vec,
+};
+
+use crate::{
+    evaluate::evaluate,
+    parser::parse,
+    reader::{Source, read_source},
+    tokenize::tokenize,
+};
 
 pub mod evaluate;
 pub mod parser;
@@ -38,19 +49,50 @@ impl From<evaluate::EvError> for IError {
     }
 }
 
-fn run() -> Result<(), IError> {
-    let source = read_source("somefile.lox").unwrap();
-    let tokens = tokenize(source).unwrap();
-    let ast = parse(tokens).unwrap();
-    let eval = evaluate(ast).unwrap();
+fn run(source: Source) -> Result<(), IError> {
+    let tokens = tokenize(source)?;
+    let ast = parse(tokens)?;
+    let eval = evaluate(ast)?;
     Ok(())
+}
+
+fn run_file(filename: &str) -> Result<(), IError> {
+    let source = reader::read_source(filename)?;
+    run(source)
+}
+
+fn run_prompt() {
+    let mut stdout = stdout();
+    let mut stdin = stdin();
+    
+    'lox: loop {
+        stdout.write_all(b">").unwrap();
+        stdout.flush().unwrap();
+        let mut buffer = String::new();
+        stdin.read_line(&mut buffer).expect("Failed to read input");
+        let source = reader::Source::new(buffer);
+
+        match run(source) {
+            Ok(_) => println!("Hexa"),
+            Err(_) => eprintln!("Nah!! Nah!!"),
+        }
+    }
 }
 
 fn main() {
     println!("Hello, Lox");
 
-    match run() {
-        Ok(_) => println!("Success!! It worked"),
-        Err(_) => println!("Failed!!, I don't know why"),
+    let args = env::args().collect::<Vec<String>>();
+    println!("Args: {:#?}", args);
+
+    if args.len() == 1 {
+        run_prompt();
+    } else if args.len() == 2 {
+        match run_file(&args[1]) {
+            Ok(_) => println!("Success!! It worked"),
+            Err(_) => println!("Failed!!, I don't know why"),
+        }
+    } else {
+        eprintln!("Usage: lox [filename]")
     }
 }
--- src/reader.rs
+++ src/reader.rs
@@ -1,18 +1,43 @@
 use std::{
     fs::File,
-    io::{Error, Read},
+    io::{self, Read},
     path::PathBuf,
 };
 
-pub struct Source {}
+pub struct Source {
+    contents: String,
+}
+
+impl Source {
+    pub fn new(contents: String) -> Self {
+      Self { contents }
+    }
+}
 
 #[derive(Debug)]
-pub struct RError {}
+pub struct RError {
+    msg: String,
+}
+
+impl From<io::Error> for RError {
+    fn from(error: io::Error) -> Self {
+        RError {
+            msg: format!("{:?}", error),
+        }
+    }
+}
 
 pub fn read_source(filename: &str) -> Result<Source, RError> {
     println!("Reading source");
+    let mut file = match File::open(filename) {
+        Ok(file) => file,
+        Err(e) => return Err(RError::from(e)),
+    };
 
-    Ok(Source {})
+    let mut contents = String::new();
+    _ = file.read_to_string(&mut contents);
+    println!("content: {}", contents);
+    Ok(Source { contents })
 }
 
 #[cfg(test)]
