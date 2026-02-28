cd71f69e2c0f3e45f10d0534d7e4c2a496013ea5
Awesome Rustacean <geniusinrust@gmail.com>
2026-02-28 17:40:17 +0300

    Implement scanning WIP

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -1,7 +1,7 @@
 use crate::reader::Source;
 use std::collections::HashMap;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum TokenType {
     // Single character tokens
     LeftParen,
@@ -49,7 +49,7 @@ pub enum TokenType {
     EOF,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Token {
     toktype: TokenType,
     lexeme: String,
@@ -58,7 +58,7 @@ pub struct Token {
     line: usize,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Literal {
     Str(String),
     Num(f64),
@@ -66,10 +66,15 @@ pub enum Literal {
 }
 
 impl Token {
-    pub fn new(toktype: TokenType, lexeme: String, line: usize, literal: Literal) -> Self {
+    pub fn new(
+        toktype: TokenType,
+        lexeme: impl Into<String>,
+        line: usize,
+        literal: Literal,
+    ) -> Self {
         Self {
             toktype,
-            lexeme,
+            lexeme: lexeme.into(),
             line,
             literal,
         }
@@ -94,6 +99,76 @@ pub fn tokenize(source: Source) -> Result<Tokens, TError> {
     let tokens = vec![];
 
     Ok(Tokens { tokens })
+}
+
+pub struct Scanner {
+    source: Vec<char>,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
+}
+
+impl Scanner {
+    fn new(source: &str) -> Self {
+        Self {
+            source: source.chars().collect(),
+            tokens: vec![],
+            start: 0,
+            current: 0,
+            line: 1,
+        }
+    }
+    pub fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+    pub fn scan_tokens(&mut self) -> Result<Tokens, TError> {
+        while !self.is_at_end() {
+            self.start = self.current;
+            self.scan_token();
+        }
+
+        self.tokens
+            .push(Token::new(TokenType::EOF, "", self.line, Literal::None));
+
+        Ok(Tokens {
+            tokens: self.tokens.clone(),
+        })
+    }
+    pub fn scan_token(&mut self) {
+        match self.advance() {
+            '(' => self.add_token(TokenType::LeftParen),
+            ')' => self.add_token(TokenType::RightParen),
+            '{' => self.add_token(TokenType::LeftBraces),
+            '}' => self.add_token(TokenType::RightBraces),
+            ',' => self.add_token(TokenType::Comma),
+            '.' => self.add_token(TokenType::Dot),
+            '-' => self.add_token(TokenType::Minus),
+            '+' => self.add_token(TokenType::Plus),
+            '*' => self.add_token(TokenType::Star),
+            ';' => self.add_token(TokenType::SemiColon),
+            _ => (),
+        }
+    }
+
+    fn advance(&mut self) -> char {
+        let c = self.source[self.current];
+        self.current += 1;
+
+        c
+    }
+
+    fn add_token_with_literal(&mut self, toktype: TokenType, literal: Literal) {
+        let text = self.source[self.start..self.current]
+            .iter()
+            .collect::<String>();
+        self.tokens
+            .push(Token::new(toktype, text, self.line, literal));
+    }
+
+    fn add_token(&mut self, toktype: TokenType) {
+        self.add_token_with_literal(toktype, Literal::None);
+    }
 }
 
 #[cfg(test)]
